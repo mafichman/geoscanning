dce8d2e875cde8adff083a222d630481ca80fec1
Michael Fichman <mfichman@gmail.com>
2020-11-24 15:46:01 -0500

    Update simple_codebase.R

--- simple_codebase.R
+++ simple_codebase.R
@@ -38,6 +38,8 @@ source("R/joinTracts.R")
 # 3. Specify the stayevent parameters - dist.threshold in meters, time.units in minutes
 # 4. Specify the time window for measuring radiusofgyration
 
+# Output is an sf object
+
 cleanData <- uploadGeodata("Data/Geotracking/multi_json_test") %>%
   cleanDates(., "2019-04-22 21:30:45", "2019-08-05 15:17:47", "file1.json") %>% # parameter
   cleanDates(., "2019-04-22 21:30:45", "2019-08-05 15:17:47", "file2.json") %>% # parameter
@@ -74,11 +76,19 @@ cleanData <- uploadGeodata("Data/Geotracking/multi_json_test") %>%
 
 # Load Retailers
 
-# Parameters - specify the location of the most recent retailer data set
+# Parameters:
+# 1. specify the location of the most recent retailer data set
 
 retailers <- intakeRetailers("Data/Retailers/all_Retailers_10_20_20.csv")
 
 # Associate retailers and Census tract info to geotracking observations
+
+# Parameters:
+# bufferAndJoin takes four parameters:
+# 1. a retailer database (retailers)
+# 2. the geotracking data (here as `.`)
+# 3. A crs (coordinate reference system) - keep default 2272 for Philadelphia area (linear unit - feet)
+# 4. A buffer size in the linear units of the crs - (defaulted below to 100 feet)
 
 cleanData_Retailers_Tracts <- cleanData %>%
   bufferAndJoin(retailers, ., 2272, 100) %>%
