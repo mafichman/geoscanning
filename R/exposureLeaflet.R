25f36338f0e4d9cf9882d17babb0937eb9b3899f
Brad Mattan <brad.mattan@gmail.com>
2021-06-09 14:41:39 -0400

    Major updates to exposureLeaflet
    Updated to accommodate larger datasets and protect privacy by selecting 5% of observations and hiding Geo IDs.

--- R/exposureLeaflet.R
+++ R/exposureLeaflet.R
@@ -1,6 +1,6 @@
 # exposureLeaflet.R
 
-# Visualizes geotracking observations subdivided by subject
+# Visualizes a random 5% sample of geotracking observations subdivided by subject
 # With stay events visualized by size and exposures in red
 # Parameters - dataSet, mph_thresh
 # dataSet needs to have gone through retailer spatial join
@@ -12,9 +12,15 @@ exposureLeaflet <- function(dataSet, mph_thresh){
   require(leaflet)
   require(leaflet.providers)
   require(leaflet.extras)
+  
+  getmode <- function(v) {
+    uniqv <- na.omit(unique(v))
+    uniqv[which.max(tabulate(match(v, uniqv)))]
+  }
+  
   dataSet <- dataSet %>% 
     as.data.frame() %>% ungroup() %>%
-    mutate(filename = as.factor(filename)) #BM: I added this because it appears to be necessary for split() in line 35.
+    mutate(filename = as.factor(filename))
   dataSet.df <- dataSet %>% 
     filter(is.na(trade_name) == FALSE & lead_lag_avg_mph < mph_thresh) %>% 
     group_by(filename, lat, lon, datetime) %>% 
@@ -24,14 +30,17 @@ exposureLeaflet <- function(dataSet, mph_thresh){
            likely_active_exposure = ifelse(datetime < expiration_date, 1, 0)) %>%
     rbind(., dataSet %>%
             filter(is.na(trade_name)== TRUE) %>%
-    mutate(is_stay_event = ifelse(is.na(stayeventgroup) == FALSE, 1, 0),
-           likely_active_exposure = 0))
+            mutate(is_stay_event = ifelse(is.na(stayeventgroup) == FALSE, 1, 0),
+                   likely_active_exposure = 0)) %>%
+    group_by(filename) %>%
+    slice_sample(prop = .05) %>% # For large datasets, trim down to a more workable number of observations.
+    ungroup()
   
   l <- leaflet() %>% 
     addProviderTiles(providers$Esri.WorldTopoMap) %>%
-    setView(lng = mean(dataSet$lon, na.rm = TRUE), mean(dataSet$lat, na.rm = TRUE), zoom = 07) %>%
+    setView(lng = getmode(dataSet.df$lon), lat = getmode(dataSet.df$lat), zoom = 11) %>%
     addScaleBar(position = "topleft")
-  dataSet.df2 = split(dataSet.df %>% as.data.frame(), dataSet.df$filename)
+  dataSet.df2 = split(dataSet.df, dataSet.df$filename)
 
   names(dataSet.df2) %>%
     purrr::walk( function(df) {
@@ -52,9 +61,10 @@ exposureLeaflet <- function(dataSet, mph_thresh){
   l %>%
     addLayersControl(
       overlayGroups = names(dataSet.df2),
-      options = layersControlOptions(collapsed = FALSE))
+      options = layersControlOptions(collapsed = TRUE)) %>%
+    hideGroup(names(dataSet.df2))
 }
 
 # Vignette
 
-# exposureLeaflet(cleanData_Retailers_Tracts)
+# exposureLeaflet(cleanData_Retailers_Tracts, mph_thresh)
