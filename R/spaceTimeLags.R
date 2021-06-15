2c326d8b7a3bf367cc1998b1f744c3a27e36e407
Brad Mattan <brad.mattan@gmail.com>
2021-06-15 13:08:53 -0400

    Update spaceTimeLags.R
    I removed arrange from the main dplyr pipe and added a check at the top of the function. Arranging just by datetime was resulting in a messed up ordering of participant IDs even though the df was just grouped by participant ID. In any case, the data should already be organized in this fashion, so I thought it made more sense to add a check/correction at the top of the function.

--- R/spaceTimeLags.R
+++ R/spaceTimeLags.R
@@ -13,26 +13,32 @@ require(tidyverse)
 require(sf)
 require(lubridate)
 
-# Convert data frame to sf
-dataframe <- dataframe %>%
-  filter(is.na(lat) == FALSE & is.na(lon) == FALSE) %>%
-  st_as_sf(., coords = c("lon", "lat"), crs = 4326, remove = FALSE) %>%
-    ungroup() %>%
-    st_transform(crs = myCRS) %>%
-    mutate(y_ft=map_dbl(geometry, ~st_centroid(.x)[[1]]),
-           x_ft=map_dbl(geometry, ~st_centroid(.x)[[2]])) %>%
-    group_by(filename)%>%
-    arrange(datetime)%>%
-    mutate(lagDist_ft = sqrt(((lag(x_ft) - x_ft)^2) + ((lag(y_ft) - y_ft)^2)),
-           lagTime = abs(datetime - lag(datetime)), 
-           lagmph = (lagDist_ft/5280) / (as.numeric(lagTime) / (60*60)),
-           leadDist_ft = sqrt(((lead(x_ft) - x_ft)^2) + ((lead(y_ft) - y_ft)^2)),
-           leadTime = abs(datetime - lead(datetime)), 
-           leadmph = (leadDist_ft/5280) / (as.numeric(leadTime) / (60*60)),
-           lead_lag_avg_mph = (leadmph + lagmph)/2,
-           mean_3_lagDist_ft = (lag(lagDist_ft, 1) + lag(lagDist_ft, 2) + lag(lagDist_ft, 3))/3) %>%
-    dplyr::select(-c(x_ft, y_ft, leadDist_ft, leadmph)) %>%
-  ungroup()
+  # Check for poorly arranged data
+  sortdata <- dataframe %>% arrange(filename, datetime)
+  if (sum(dataframe$datetime!=sortdata$datetime)>0) {
+    message("Data were not sorted first by participant ID and then by date/time. spaceTimeLags has now sorted the data accordingly.")
+    dataframe <- dataframe %>% arrange(filename, datetime)
+  }
+  
+  # Convert data frame to sf
+  dataframe <- dataframe %>%
+    filter(is.na(lat) == FALSE & is.na(lon) == FALSE) %>%
+    st_as_sf(., coords = c("lon", "lat"), crs = 4326, remove = FALSE) %>%
+      ungroup() %>%
+      st_transform(crs = myCRS) %>%
+      mutate(y_ft=map_dbl(geometry, ~st_centroid(.x)[[1]]),
+             x_ft=map_dbl(geometry, ~st_centroid(.x)[[2]])) %>%
+      group_by(filename) %>%
+      mutate(lagDist_ft = sqrt(((lag(x_ft) - x_ft)^2) + ((lag(y_ft) - y_ft)^2)),
+             lagTime = abs(datetime - lag(datetime)), 
+             lagmph = (lagDist_ft/5280) / (as.numeric(lagTime) / (60*60)),
+             leadDist_ft = sqrt(((lead(x_ft) - x_ft)^2) + ((lead(y_ft) - y_ft)^2)),
+             leadTime = abs(datetime - lead(datetime)), 
+             leadmph = (leadDist_ft/5280) / (as.numeric(leadTime) / (60*60)),
+             lead_lag_avg_mph = (leadmph + lagmph)/2,
+             mean_3_lagDist_ft = (lag(lagDist_ft, 1) + lag(lagDist_ft, 2) + lag(lagDist_ft, 3))/3) %>%
+      dplyr::select(-c(x_ft, y_ft, leadDist_ft, leadmph)) %>%
+    ungroup()
 
 return(dataframe)
 }
